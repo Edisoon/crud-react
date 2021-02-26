8b660fdb6ae52510bf0ae8413d1f1d2de5e32e02
Edisoon <Edison_theM3ka@hotmail.com>
2021-02-26 01:09:26 -0400

    add task memory
    agregar tarea en la memoria del navegdor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { isEmpty } from 'lodash';
+import { FaTrashAlt, FaPen, FaSave } from 'react-icons/fa';
+import shortid from 'shortid';
 
 function App() {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (e) => {
+    e.preventDefault()
+    if(isEmpty(task)){
+      console.log('Task Empty')
+      return
+    }
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([...tasks, newTask])
+
+    setTask('')
+
+  }
   return (
     <div className="container mt-5">
       <h1>Tareas</h1>
       <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de Tareas</h4>
+          <ul className="list-group">
+
+          {
+            tasks.map((task) =>(
+
+              <li className="list-group-item" key={task.id}>
+              <span className="lead">{task.name}</span>
+              <button className="btn btn-outline-danger btn-sm float-right mx-1"><FaTrashAlt/></button>
+              <button className="btn btn-outline-warning btn-sm float-right"><FaPen/></button>
+              </li>
+            ))
+          }
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Agregar Tarea</h4>
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Ingresa la tarea.."
+              onChange={(text) => setTask(text.target.value)}
+              value={task}
+            />
+            <button className="btn btn-outline-dark btn-block"
+            type="submit">Guardar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
